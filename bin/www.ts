53e00c2f73e33d292503565591176dc516eaff99
Kimi Päivärinta <mail@kimin.info>
2022-01-10 08:51:59 +0200

    Typescript version

--- bin/www.ts
+++ bin/www.ts
@@ -1,12 +1,10 @@
-#!/usr/bin/env node
-
 /**
  * Module dependencies.
  */
-
-var app = require('../app');
-var debug = require('debug')('express-test:server');
-var http = require('http');
+import app from '../app'
+import debugLib from 'debug';
+import http from 'http'
+const debug = debugLib('express-test:server');
 
 /**
  * Get port from environment and store in Express.
@@ -33,7 +31,7 @@ server.on('listening', onListening);
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
+function normalizePort(val: string) {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -53,7 +51,7 @@ function normalizePort(val) {
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
+function onError(error: any) {
   if (error.syscall !== 'listen') {
     throw error;
   }
